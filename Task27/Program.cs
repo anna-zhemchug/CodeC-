6983c3d4b6ddd415c4a142449388517441d62dc0
Anna Zhemchugova <mio007@mail.ru>
2022-09-03 22:14:27 +0300

    Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите функцию и запустите ее, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+int SumNum(int N)
+{
+    int sum = 0;
+    while (N != 0)
+    {
+        sum += N % 10;
+        N /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine(SumNum(num));
